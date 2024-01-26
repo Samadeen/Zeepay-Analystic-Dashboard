52ffcc0e6af8b2d3d3db8476313c35a963e005bb
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-26 23:10:06 +0100

    Create types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,17 @@
+type LastOrder = {
+  id: string;
+  img: string;
+  name: string;
+  date: string;
+  amount: string;
+  status: string;
+};
+
+type Card = {
+  img: string;
+  title: string;
+  amount: number;
+  graph: string;
+  trend: string;
+  rate: string;
+};
