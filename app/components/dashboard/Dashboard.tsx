bb0f966abc0cf129bd48f530671c8170c825b31d
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-27 19:26:16 +0100

    Update Dashboard.tsx

--- app/components/dashboard/Dashboard.tsx
+++ app/components/dashboard/Dashboard.tsx
@@ -5,12 +5,19 @@ import Tabs from '../tabs/Tabs';
 import Order from '../orders/Order';
 import Platforms from '../platforms/Platforms';
 import { useTheme } from '@/app/utils/theme-context';
+import { mobileNav } from '@/app/constant/data';
+import Image from 'next/image';
 
 const Dashboard = () => {
   const { theme } = useTheme();
   return (
     <div id={theme} className={styles.dashboard_container}>
       <Header />
+      <div className={styles.mobile_nav}>
+        {mobileNav.map((mob) => {
+          return <Image src={mob.img} alt={`${mob.tooltip}`} />;
+        })}
+      </div>
       <div className={styles.top_content}>
         <Chart />
         <Tabs />
