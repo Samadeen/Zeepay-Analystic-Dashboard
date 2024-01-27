a54b7fd5ea700ae0f705721c4331065e7605e827
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-27 15:41:28 +0100

    Update Sidebar.tsx

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,35 +1,71 @@
+'use client';
+
 import { sidebarBottom, sidebarTop } from '@/app/constant/data';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '@/public/assets/logo.svg';
 import styles from './Sidebar.module.scss';
 import brightness from '@/public/assets/brightness.svg';
 import moon from '@/public/assets/moon.svg';
+import { motion as m } from 'framer-motion';
+import { useTheme } from '@/app/utils/theme-context';
 
 const Sidebar = () => {
+  const { theme, toggleTheme } = useTheme();
+  const [switcher, setSwitcher] = useState(false);
+
+  const switchHandler = () => {
+    setSwitcher((prevState) => !prevState);
+    toggleTheme();
+  };
+
   return (
-    <section className={styles.sidebar_container}>
+    <section
+      id={theme}
+      className={styles.sidebar_container}
+      style={
+        theme === 'dark'
+          ? { backgroundColor: '#f7f8fa' }
+          : { backgroundColor: '#0f172a' }
+      }
+    >
       <Image src={logo} alt='logo' className={styles.head_logo} />
       <ul>
         {sidebarTop.map((side) => {
           return (
-            <li>
+            <li key={side.tooltip}>
               <Image src={side.img} alt={side.tooltip} />
-              <p>{side.tooltip}</p>
+              <p
+                style={
+                  theme === 'dark'
+                    ? { backgroundColor: '#0f172a', color: '#f7f8fa' }
+                    : { backgroundColor: '#34caa5', color: '#fff' }
+                }
+              >
+                {side.tooltip}
+              </p>
             </li>
           );
         })}
         <li className={styles.theme}>
-          <Image src={brightness} alt='brightness' />
-          <Image src={moon} alt='darkness' />
+          <Image src={brightness} alt='brightness' onClick={switchHandler} />
+          <Image src={moon} alt='darkness' onClick={switchHandler} />
         </li>
       </ul>
       <ul>
         {sidebarBottom.map((side) => {
           return (
-            <li>
+            <li key={side.tooltip}>
               <Image src={side.img} alt={side.tooltip} />
-              <p>{side.tooltip}</p>
+              <p
+                style={
+                  theme === 'dark'
+                    ? { backgroundColor: '#0f172a', color: '#f7f8fa' }
+                    : { backgroundColor: '#34caa5', color: '#fff' }
+                }
+              >
+                {side.tooltip}
+              </p>
             </li>
           );
         })}
