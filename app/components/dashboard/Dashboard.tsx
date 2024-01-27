ddd38f6edceee90d1345c996fa53ba9d7c77394d
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-27 01:23:08 +0100

    Update Dashboard.tsx

--- app/components/dashboard/Dashboard.tsx
+++ app/components/dashboard/Dashboard.tsx
@@ -1,10 +1,16 @@
 import styles from './Dashboard.module.scss';
 import Header from '../header/Header';
+import Chart from '../chart/Chart';
+import Tabs from '../tabs/Tabs';
 
 const Dashboard = () => {
   return (
     <div className={styles.dashboard_container}>
       <Header />
+      <div className={styles.top_content}>
+        <Chart />
+        <Tabs />
+      </div>
     </div>
   );
 };
