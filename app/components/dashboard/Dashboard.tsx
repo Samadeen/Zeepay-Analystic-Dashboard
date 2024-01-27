33e251f4b835091ba9ec10e99f0b305799c67c61
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-27 02:18:48 +0100

    Update Dashboard.tsx

--- app/components/dashboard/Dashboard.tsx
+++ app/components/dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import styles from './Dashboard.module.scss';
 import Header from '../header/Header';
 import Chart from '../chart/Chart';
 import Tabs from '../tabs/Tabs';
+import Order from '../orders/Order';
 
 const Dashboard = () => {
   return (
@@ -10,6 +11,9 @@ const Dashboard = () => {
       <div className={styles.top_content}>
         <Chart />
         <Tabs />
+      </div>
+      <div className={styles.bottom_content}>
+        <Order />
       </div>
     </div>
   );
