6b5a425bcb4db60125627acf0d805aae1e3af538
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-27 01:22:51 +0100

    Create Cards.tsx

---
+++ app/components/cards/Cards.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import styles from './Cards.module.scss';
+
+const Cards = ({ img, graph, title, amount, trend, rate, up }: Card) => {
+  return (
+    <section className={styles.card_container}>
+      <div className={styles.top}>
+        <Image src={img} alt={`${img}`} />
+        <Image src={graph} alt={`${graph}`} />
+      </div>
+      <div className={styles.mid}>
+        <h3>{title}</h3>
+        <h2>{amount}</h2>
+      </div>
+      <div className={styles.bom}>
+        <div
+          className={styles.trend}
+          style={
+            up
+              ? {
+                  background: 'rgba(52, 202, 165, 0.12)',
+                }
+              : {
+                  background: 'rgba(237, 84, 78, 0.12)',
+                }
+          }
+        >
+          <Image src={trend} alt={`${trend}`} />
+          <small
+            style={
+              up
+                ? {
+                    color: '#34CAA5',
+                  }
+                : {
+                    color: '#ED544E',
+                  }
+            }
+          >
+            {rate}
+          </small>
+        </div>
+        <p>vs. previous month</p>
+      </div>
+    </section>
+  );
+};
+
+export default Cards;
