78b5a8adb17c921a846c89b12725de337ed2b416
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-27 15:41:40 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import Dashboard from './components/dashboard/Dashboard';
 import Sidebar from './components/sidebar/Sidebar';
+import { useTheme } from './utils/theme-context';
 
 export default function Home() {
+  const { theme } = useTheme();
   return (
-    <main>
+    <main id={theme}>
       <div className=''>
         <Sidebar />
         <Dashboard />
